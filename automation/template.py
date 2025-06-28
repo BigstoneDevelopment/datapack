b7645aa03cdc5f11ae9c944d081056be1021e13c
guigui0246 <guigui0246@yahoo.com>
2025-06-28 16:14:33 +0200

    WIP: [automation] add validation methods and fix template replacement syntax

--- automation/template.py
+++ automation/template.py
@@ -1,6 +1,18 @@
 
-from typing import Self
+import re
+from typing import Literal, Self
 import os
+
+
+AllValidComponentsTemplate = Literal[
+    "structure_file_name",
+    "structure_display_name",
+    "structure_author",
+    "structure_description",
+    "ports",
+    "namespace",
+    "model"
+]
 
 
 class Template:
@@ -11,9 +23,15 @@ class Template:
     def __replace__(self, **kwargs: str) -> Self:
         result = self.content
         for key, value in kwargs.items():
-            result = result.replace(f"{{{{ {key} }}}}", value)
+            result = result.replace(f"<{key}>", value)
         self.content = result
         return self
+
+    def valid(self) -> list[AllValidComponentsTemplate]:
+        return re.findall(r"<(\w+)>", self.content)
+
+    def finished(self) -> bool:
+        return not re.search(r"<\w+>", self.content)
 
 
 TEMPLATES: dict[str, Template] = {}
@@ -26,3 +44,6 @@ if os.path.exists(templates_dir):
             with open(file_path, "r") as template_file:
                 content = template_file.read()
                 TEMPLATES[filename] = Template(content)
+
+
+__all__ = ["Template", "TEMPLATES"]
